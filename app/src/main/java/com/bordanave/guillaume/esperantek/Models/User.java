fe21eeb1a7fbbca36004486fba5edde7d49f4468
guillaume <guillaume.bordanave@gmail.com>
2017-03-09 12:26:34 +0100

    init

---
+++ app/src/main/java/com/bordanave/guillaume/esperantek/Models/User.java
@@ -0,0 +1,12 @@
+package com.bordanave.guillaume.esperantek.Models;
+
+/**
+ * Created by guillaume on 09/03/2017.
+ */
+
+public class User {
+    private String email;
+
+    private String password;
+
+}
